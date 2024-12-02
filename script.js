3f05a4b01f573f5ffa0837275983792a24eaf14e
Normann Blum <normannblum@gmail.com>
2024-12-02 12:34:13 +0100

    add icons and new images

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
 // "async function" ruft die Daten der Pokemons von der API ab
 async function fetchPokemonData() {
-  // Initialisierung eines leeren Arrays, das später die Pokémon-Daten speichert.
+  // Initialisiere ein leeres Array um die Pokémon-Daten zu speichern
   let pokemonList = [];
 
-  // Schleife um die ersten 20 Pokémon zu durchlaufen
-  for (let i = 1; i < 50; i++) {
+  for (let i = 1; i < 20; i++) {
     // Abrufen der Daten eines Pokémon von der PokeAPI anhand seiner ID.
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-    // Konvertieren der API-Antwort in ein JSON-Objekt.
+
+    // Konvertiere die API-Antwort in ein JSON-Objekt
     let pokemon = await response.json();
-    // Hinzufügen eines Pokémon-Objekts mit Informationen (Name, Typ, Bild, ID) zum Array.
+
+    // Hinzufügen eines Pokemon-Objekts mit Informationen (Name, Typ, Bild, ID) zum Array.
     pokemonList.push({
       name: pokemon.name,
       type: pokemon.types[0].type.name,
-      image: pokemon.sprites.front_default,
+      image: pokemon.sprites.other["official-artwork"].front_default,
       id: pokemon.id,
     });
   }
 
-  // Sobald die Daten geladen sind, werden die Pokemon Daten dargestellt.
   renderPokemonCards(pokemonList);
 }
 
 // Funktion zur Darstellung der PokemonDaten auf der Webseite.
 function renderPokemonCards(pokemonList) {
-  // Hole das HTML-Element ID "content", wo die Karten eingefügt werden sollen.
+  // Hole das HTML-Element ID "content", wo die Karten eingefügt werden sollen
   let contentElement = document.getElementById("content");
+
   // Initialisierung eines leeren Strings, der später die HTML-Karten enthält.
   let cardsHTML = "";
 
-  // Schleife die durch die Liste der Pokémon iteriert.
+  // Schleife durch die Liste der Pokemon, um jede Karte zu erstellen
   for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon = pokemonList[i]; // Das aktuelle Pokémon.
+    let pokemon = pokemonList[i]; // Das aktuelle Pokemon
 
-    // Fügt HTML-Code für eine Pokémon-Karte zum cardsHTML-String hinzu.
+    // HTML-Struktur für eine einzelne Pokemon-Karte
     cardsHTML += `
-          <div class="pokemon-card ${pokemon.type}"> 
-            <h2>${pokemon.name} (#${pokemon.id})</h2> 
-            <img src="${pokemon.image}">
-            <p>Type: ${pokemon.type}</p> 
-          </div>
-        `;
+      <div class="pokemon-card ${pokemon.type}"> 
+        <div class="card-header"> 
+          <span class="pokemon-id">#${pokemon.id}</span>
+          <span class="pokemon-name">${pokemon.name}</span> 
+        </div>
+        <div class="card-body"> 
+          <img class="pokemon-image" src="${pokemon.image}"> 
+        </div>
+        <div class="card-footer"> 
+          <img class="type-icon" src="./assets/icons/${pokemon.type}.png">
+        </div>
+      </div>
+    `;
   }
 
   // Fügt den generierten HTML-Code in das "content"-Element ein.
